3fabc293632c81d43a7f7ddaab97ac9a93bcdb9f
gustavoabell <gustavoabel.contato@gmail.com>
2022-05-08 21:39:59 -0300

    🔥 feat: add feedback function handleFeedback

--- web/src/components/Widget/WidgetForm/index.tsx
+++ web/src/components/Widget/WidgetForm/index.tsx
@@ -1,60 +1,75 @@
-import { CloseButton } from "../CloseButton";
+import { useState } from 'react';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
 
-import bugImageUrl from "../../assets/bug.svg";
-import ideaImageUrl from "../../assets/idea.svg";
-import thoughtImageUrl from "../../assets/thought.svg";
-import { useState } from "react";
-import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
-import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import bugImageUrl from '../../../assets/bug.svg';
+import ideaImageUrl from '../../../assets/idea.svg';
+import thoughtImageUrl from '../../../assets/thought.svg';
 
-export const feedbackTypes = { 
-  BUG: {
-    title: "Problema",
+export const feedbackTypes = {
+  'BUG': {
     image: {
-    source: bugImageUrl,
-    alt: 'Imagem de um inseto',
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+    title: 'Problema'
   },
-  },
-  IDEA: {
-    title: "Ideia",
+  'IDEA': {
     image: {
-    source: ideaImageUrl,
-    alt: 'Imagem de uma lâmpada',
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+    title: 'Ideia'
   },
-  },
-  OTHER: {
-    title: "Outro",
+  'OTHER': {
     image: {
-    source: thoughtImageUrl,
-    alt: 'Imagem de um balão de pensamento'
-  }
-  }
-}
+      source: thoughtImageUrl,
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+    title: 'Outro'
+  },
+};
 
 export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
-  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false)
+
+  function handleFeedbackSent() {
+    setFeedbackSent(true);
+  }
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-      <span className="text-xl leading-6">Deixe seu feedback</span>
-      
-      
-      <CloseButton />
-      </header>
-
-     {!feedbackType ? (
-      <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-     ) : (
-       <FeedbackContentStep />
-     )}
-      
-          
-     
+      { feedbackSent ? (
+        <FeedbackSuccessStep 
+          onSendAnotherFeedbackRequest={handleRestartFeedback}
+        />
+      ) : (
+        <>
+          { !feedbackType ? (
+            <FeedbackTypeStep 
+              onFeedbackTypeChanged={setFeedbackType}
+            />
+          ) : (
+            <FeedbackContentStep 
+              feedbackType={feedbackType} 
+              onFeedbackCanceled={() => setFeedbackType(null)}
+              onFeedbackSent={handleFeedbackSent}
+            />
+          )}
+        </>
+      )}
 
       <footer className="text-xs text-neutral-400">
-      Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+        Feito com ♥ pela <a className="underline underline-offset-1" target="_blank" href="https://rocketseat.com.br">Rocketseat</a>
       </footer>
     </div>
   );
